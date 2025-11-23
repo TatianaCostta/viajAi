bc75c21461c7b9333d133dedc124cadac288989d
elaineaquino22 <naynepri@gmail.com>
2025-11-23 13:56:14 -0300

    Criação do modal criar post

--- src/app/components/menu-nav/menu-nav.ts
+++ src/app/components/menu-nav/menu-nav.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+// Importe o modal que acabamos de criar
+import { ModalCriarPostComponent } from '../modal-criar-post/modal-criar-post';
 
 @Component({
   selector: 'app-menu-nav',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterLinkActive], 
-  // Atenção: verifique se os arquivos HTML e CSS também estão com nomes curtos na pasta
-  templateUrl: './menu-nav.html', 
+  imports: [CommonModule, RouterLink, RouterLinkActive, ModalCriarPostComponent], // <--- Adicione aqui
+  templateUrl: './menu-nav.html',
   styleUrls: ['./menu-nav.css']
 })
-export class MenuNavComponent { }
+export class MenuNavComponent {
+    // Variável de controle
+    mostrarModal = false;
+
+    abrirModal() {
+        this.mostrarModal = true;
+    }
+
+    fecharModal() {
+        this.mostrarModal = false;
+    }
+}
--- src/app/components/modal-criar-post/modal-criar-post.ts
+++ src/app/components/modal-criar-post/modal-criar-post.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-modal-criar-post',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './modal-criar-post.html',
-  styleUrl: './modal-criar-post.css',
+  styleUrls: ['./modal-criar-post.css']
 })
-export class ModalCriarPost {
+export class ModalCriarPostComponent {
+  // Cria um evento para avisar quem chamou que é hora de fechar
+  @Output() fechar = new EventEmitter<void>();
 
+  fecharModal() {
+    this.fechar.emit();
+  }
 }
