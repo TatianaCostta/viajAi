50459ecba49ce9452f8748fd4b04da165d2d7a18
elaineaquino22 <naynepri@gmail.com>
2025-11-23 14:19:38 -0300

    Criação do menu explorar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,15 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home';
 import { LoginComponent } from './pages/login/login';
 import { PerfilComponent } from './pages/perfil/perfil';
+// 1. Importar o novo componente
+import { ExplorarComponent } from './pages/explorar/explorar'; 
 
 export const routes: Routes = [
-  // Se o usuário entrar apenas em "meusite.com", joga ele para "meusite.com/home"
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-
-  // Agora sim a rota 'home' existe para o menu funcionar
   { path: 'home', component: HomeComponent },
-  
   { path: 'login', component: LoginComponent },
-  { path: 'perfil', component: PerfilComponent }
+  { path: 'perfil', component: PerfilComponent },
+  
+  // 2. Adicionar a rota explorar
+  { path: 'explorar', component: ExplorarComponent } 
 ];
