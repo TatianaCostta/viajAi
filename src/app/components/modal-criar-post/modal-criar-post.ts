4ec6bf0e12c6c4c19949e663ee7e1cb070015d76
elaineaquino22 <naynepri@gmail.com>
2025-11-27 00:31:33 -0300

    Ajustes finos das demais telas(falta icones da tela de perfil

--- src/app/components/modal-criar-post/modal-criar-post.ts
+++ src/app/components/modal-criar-post/modal-criar-post.ts
@@ -9,10 +9,20 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./modal-criar-post.css']
 })
 export class ModalCriarPostComponent {
-  // Cria um evento para avisar quem chamou que é hora de fechar
   @Output() fechar = new EventEmitter<void>();
 
   fecharModal() {
     this.fechar.emit();
+  }
+
+  // Função que roda quando o usuário escolhe uma foto
+  aoSelecionarArquivo(event: any) {
+    const arquivo = event.target.files[0];
+    
+    if (arquivo) {
+      console.log("Arquivo selecionado:", arquivo.name);
+      // Aqui futuramente você pode colocar a lógica para exibir a foto na tela
+      alert(`Você selecionou: ${arquivo.name}`); // Apenas para testar visualmente
+    }
   }
 }
